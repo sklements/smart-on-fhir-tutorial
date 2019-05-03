3ed519b3add74062579dc2efe0dc034ac45eb3e3
Scott Klements <sklements@holonsolutions.com>
2019-05-03 13:06:57 -0400

    Comment window redirect to mirth channel for now.

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,88 +1,87 @@
-(function(window) {
-  window.extractData = function() {
-    var ret = $.Deferred();
+(function (window) {
+	window.extractData = function () {
+		var ret = $.Deferred();
 
-    function onError() {
-      console.log('Loading error', arguments);
-      ret.reject();
-    }
+		function onError() {
+			console.log('Loading error', arguments);
+			ret.reject();
+		}
 
-    function onReady(smart) {
-    	console.log('smart: ' + JSON.stringify(smart));
-    	
-//        $('#fhirid').html(smart.tokenResponse.patient);
+		function onReady(smart) {
+			console.log('smart: ' + JSON.stringify(smart));
 
-        var patient = smart.patient;
-        var pt = patient.read();
-      
-        var practObj = {};
-        practObj.type = "Practitioner";
-        practObj.id = "605926"; // smart.tokenResponse.user;
-        $('#userId').html(smart.tokenResponse.user);
-        
-        var pract = smart.api.read( practObj );
+			//        $('#fhirid').html(smart.tokenResponse.patient);
 
-        $.when(pt, pract).fail(onError);
-        $.when(pt, pract).done(function (patient, practitioner) {
+			var patient = smart.patient;
+			var pt = patient.read();
 
-          var pt = defaultPatient();
-          pt.id = patient.id;
-          var identifiers = patient.identifier;
-          
-          for( var j=0 ; j<identifiers.length ; j++ )
-          {
-            if( identifiers[j].type.coding !== undefined
-            	&& identifiers[j].type.coding.length > 0
-            	&& identifiers[j].type.coding[0].code == "MR" )
-            {
-               pt.mrn = identifiers[j].value;
-            }
-          }          
+			var practObj = {};
+			practObj.type = "Practitioner";
+			practObj.id = "605926"; // smart.tokenResponse.user;
+			$('#userId').html(smart.tokenResponse.user);
 
-          var pract = {};
-          pract.id = practitioner.id;
-          pract.npi = "";
-          identifiers = practitioner.data.identifier;
-          
-          for( var i=0 ; i<identifiers.length ; i++ )
-          {
-              if( identifiers[i].type.coding !== undefined
-        		  && identifiers[i].type.coding.length > 0
-        		  && identifiers[i].type.coding[0].code == "PRN" )
-              {
-            	  pract.npi = identifiers[i].value;
-              }
-          }
+			var pract = smart.api.read(practObj);
 
-          if( pt.mrn != "" && pract.npi != "" )
-          {
-        	  var url = "http://localhost:1080/cernercontext/?partnerId=999999999999999-9999999999999&mrn=" + pt.mrn + "&npi=" +pract.npi;
-        	  window.location = url;
-        	  console.log('launch Mirth Cerner Context channel: ' + url);
-          }
+			$.when(pt, pract).fail(onError);
+			$.when(pt, pract).done(function (patient, practitioner) {
 
-          ret.resolve(pt, pract);
-        });
-    }
+				var pt = defaultPatient();
+				pt.id = patient.id;
+				var identifiers = patient.identifier;
 
-    FHIR.oauth2.ready(onReady, onError);
+				for (var j = 0; j < identifiers.length; j++) {
+					if (identifiers[j].type.coding !== undefined
+						 && identifiers[j].type.coding.length > 0
+						 && identifiers[j].type.coding[0].code == "MR") {
+						pt.mrn = identifiers[j].value;
+					}
+				}
 
-    return ret.promise();
-  };
+				var pract = {};
+				pract.id = practitioner.id;
+				pract.npi = "";
+				identifiers = practitioner.data.identifier;
 
-  function defaultPatient() {
-    return {
-      id: {value: ''},
-      mrn: {value: ''},
-    };
-  }
+				for (var i = 0; i < identifiers.length; i++) {
+					if (identifiers[i].type.coding !== undefined
+						 && identifiers[i].type.coding.length > 0
+						 && identifiers[i].type.coding[0].code == "PRN") {
+						pract.npi = identifiers[i].value;
+					}
+				}
 
-  window.drawVisualization = function(pt, pract) {
+				if (pt.mrn != "" && pract.npi != "") {
+					var url = "http://localhost:1080/cernercontext/?partnerId=999999999999999-9999999999999&mrn=" + pt.mrn + "&npi=" + pract.npi;
+					//        	  window.location = url;
+					console.log('launch Mirth Cerner Context channel: ' + url);
+				}
 
-    $('#holder').show();
-    $('#patientId').html(pt.id);
-    $('#mrn').html(pt.mrn);
-    $('#npi').html(pract.npi);
-  };
+				ret.resolve(pt, pract);
+			});
+		}
+
+		FHIR.oauth2.ready(onReady, onError);
+
+		return ret.promise();
+	};
+
+	function defaultPatient() {
+		return {
+			id: {
+				value: ''
+			},
+			mrn: {
+				value: ''
+			},
+		};
+	}
+
+	window.drawVisualization = function (pt, pract) {
+
+		$('#holder').show();
+		$('#patientId').html(pt.id);
+		$('#mrn').html(pt.mrn);
+		$('#npi').html(pract.npi);
+	};
 
 })(window);
