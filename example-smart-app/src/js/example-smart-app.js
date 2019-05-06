355c56609936b5356e97562d89de1974cba46f37
Scott Klements <sklements@holonsolutions.com>
2019-05-06 12:05:16 -0400

    Handle missing identifiers for patient and practitioner.

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -36,14 +36,17 @@
 
 				context.patient = defaultPatient();
 				context.patient.id = patient.id;
+				context.patient.mrn = patient.id;
 				var identifiers = patient.identifier;
 
-				for (var j = 0; j < identifiers.length; j++) {
-					if (identifiers[j].type.coding !== undefined
-						 && identifiers[j].type.coding.length > 0
-						 && identifiers[j].type.coding[0].code == "MR"
-						 && identifiers[j].type.text.search(/^MRN$/i) !== -1) {
-						context.patient.mrn = identifiers[j].value;
+				if (identifiers !== undefined) {
+					for (var j = 0; j < identifiers.length; j++) {
+						if (identifiers[j].type.coding !== undefined
+							 && identifiers[j].type.coding.length > 0
+							 && identifiers[j].type.coding[0].code == "MR"
+							 && identifiers[j].type.text.search(/^MRN$/i) !== -1) {
+							context.patient.mrn = identifiers[j].value;
+						}
 					}
 				}
 
@@ -52,12 +55,14 @@
 				context.practitioner.npi = practitioner.id;
 				identifiers = practitioner.identifier;
 
-				for (var i = 0; i < identifiers.length; i++) {
-					if (identifiers[i].type.coding !== undefined
-						 && identifiers[i].type.coding.length > 0
-						 && identifiers[i].type.coding[0].code == "PRN"
-						 && identifiers[i].type.text.search(/^National Provider Identifier$/i) !== -1) {
-						context.practitioner.npi = identifiers[i].value;
+				if (identifiers !== undefined) {
+					for (var i = 0; i < identifiers.length; i++) {
+						if (identifiers[i].type.coding !== undefined
+							 && identifiers[i].type.coding.length > 0
+							 && identifiers[i].type.coding[0].code == "PRN"
+							 && identifiers[i].type.text.search(/^National Provider Identifier$/i) !== -1) {
+							context.practitioner.npi = identifiers[i].value;
+						}
 					}
 				}
 
